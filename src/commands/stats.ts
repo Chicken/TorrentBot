2a4d5de4671adb5c52de072325db3530c75e4089
Antti Ellilä <antti@antti.codes>
2026-03-02 18:37:09 +0200

    chore: add speeds to stats

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -12,8 +12,8 @@ export async function run(command: ChatInputCommandInteraction) {
         [
             "```",
             `Status   | ${stats.server_state?.connection_status}`,
-            `Upload   | ${prettySize(stats.server_state?.alltime_ul ?? 0)}`,
-            `Download | ${prettySize(stats.server_state?.alltime_dl ?? 0)}`,
+            `Upload   | ${prettySize(stats.server_state?.alltime_ul ?? 0)} @ ${prettySize(stats.server_state?.up_info_speed ?? 0, true)}`,
+            `Download | ${prettySize(stats.server_state?.alltime_dl ?? 0)} @ ${prettySize(stats.server_state?.dl_info_speed ?? 0, true)}`,
             `Ratio    | ${stats.server_state?.global_ratio}`,
             `Peers    | ${stats.server_state?.total_peer_connections}`,
             `Torrents | ${Object.values(stats.torrents ?? {}).length}`,
